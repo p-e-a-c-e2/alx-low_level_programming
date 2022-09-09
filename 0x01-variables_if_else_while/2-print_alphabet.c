3d9bb79bbe55e001b5ea42525ef814286019ef74
p-e-a-c-e2 <smartpeace6@gmail.com>
2022-09-09 16:30:00 -0700

    2-print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - prints lowercase letters
+ *
+ *Return: 0 if true
+ */
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}

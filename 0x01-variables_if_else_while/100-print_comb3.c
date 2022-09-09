8903ba1777e64b8f46547c3ba156a1825d3b6815
p-e-a-c-e2 <smartpeace6@gmail.com>
2022-09-09 16:50:05 -0700

    100-print

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - prints possible different combo of 2 digits
+ *
+ *Return: 0 if true
+ */
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i < 9; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+putchar((i % 10) + '0');
+putchar((j % 10) + '0');
+if (i == 8 && j == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}

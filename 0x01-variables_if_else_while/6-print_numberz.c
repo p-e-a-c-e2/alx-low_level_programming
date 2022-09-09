3a7196b47caedfc7bad7042075a34fd45a07be9a
p-e-a-c-e2 <smartpeace6@gmail.com>
2022-09-09 16:40:32 -0700

    6-prints

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - prints digits of base 10
+ *
+ *Return: 0 if true
+ */
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}

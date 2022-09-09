dae4317c9ab15bba6dcc4af6e98ec44312f925e1
p-e-a-c-e2 <smartpeace6@gmail.com>
2022-09-09 16:38:23 -0700

    5-print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - prints all numbers of base 10
+ *Return: 0 if true
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+printf("%d", i);
+printf("\n");
+return (0);
+}

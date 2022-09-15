88fc8807d8ae8034e36cac34ca84ff7cba926ea8
p-e-a-c-e2 <smartpeace6@gmail.com>
2022-09-15 16:31:10 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagnol line.
+ *
+ * @n: number of times to print \.
+ */
+void print_diagonal(int n)
+{
+	int x, y;
+
+	for (x = 1; x <= n; x++)
+	{
+		for (y = 1; y <= x; y++)
+		{
+			if (x >= 2 && y <= x - 1)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\\');
+			}
+		}
+		if (x == n)
+		{
+			break;
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}

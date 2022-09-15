b5623409b254eea63cdf88564352409de5f68cdc
p-e-a-c-e2 <smartpeace6@gmail.com>
2022-09-15 16:16:37 -0700

    more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ *
+ *  *more_numbers - prints x10 numbers 0-14
+ *
+ *   */
+
+void more_numbers(void)
+
+{
+
+	int x, y;
+
+	for (y = 0; y < 10; y++)
+
+	{
+
+		x = 0;
+
+		while (x <= 14)
+
+		{
+
+			if (x >= 10)
+
+			{
+
+				_putchar(x / 10 + '0');
+
+			}
+
+			_putchar(x % 10 + '0');
+
+			x++;
+
+		}
+
+		_putchar('\n');
+
+	}
+
+}

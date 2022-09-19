1aa0daa78a22da7d9f8be951bd69cd757dc68ccf
p-e-a-c-e2 <smartpeace6@gmail.com>
2022-09-19 06:23:42 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *reset_to_98 - takes a pointer to an int as para & updates the value to 98
+ *@n: pointer to be checked
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+Footer
+© 2022 GitHub, Inc.

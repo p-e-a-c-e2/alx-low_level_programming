01d020181f988011546995cb031151b45e69fba3
p-e-a-c-e2 <smartpeace6@gmail.com>
2022-09-19 07:57:18 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *puts_half - prints second half of the string
+ *@str: string to be checked
+ */
+void puts_half(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+;
+i++;
+for (i /= 2; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
